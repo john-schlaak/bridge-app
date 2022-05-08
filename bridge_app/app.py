4bd61fbbbb56b75151d89bbc1edc39b7d4129103
isaacbstone <46686775+isaacbstone@users.noreply.github.com>
2022-05-08 10:16:25 -0500

    Update app.py

--- bridge_app/app.py
+++ bridge_app/app.py
@@ -11,7 +11,14 @@ app.config['CORS_HEADERS'] = 'Content-Type, X-XSRF-TOKEN'
 def generate():
     data = request.get_json()
     ## BEGIN YOUR CODE
-    output = "\ngraph TD\nA[Client] --> B[Load Balancer]\nB --> C[Server1]\nB --> D[Server2]" # PLACEHOLDER
+#     output = "\ngraph TD\nA[Client] --> B[Load Balancer]\nB --> C[Server1]\nB --> D[Server2]" # PLACEHOLDER
+    output = ""
+    for node in bidNodes:
+        node = str(node.id) + "{"+str(node.bidName)+"}"
+        output+=node+"\n"
+        for rule in node.rules:
+            rule = str(node.id)+"--> |"+"HCP: "+ str(rule.HCPMin)+"-"+str(rule.HCPMax) + "\nTP: "+ str(rule.TPMin)+"-"+str(rule.TPMax) {+" "+str(group.cards)+" "+str(group.num)for group in rule.groups}"|" +rule.nextBid
+            ouput+=rule+"\n"
     print(data) # PLACEHOLDER
     ## END YOUR CODE
     return output
