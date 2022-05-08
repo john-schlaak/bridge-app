1e01668480aeaaac108241cc2a713c6363707a69
ThisIsAName12345 <schlaakja@gmail.com>
2022-05-08 10:14:48 -0500

    reworked app.py

--- bridge_app/app.py
+++ bridge_app/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type, X-XSRF-TOKEN'
 
-@app.route("/generate", methods=["POST"])
+@app.route("/generate", methods=["POST", "OPTIONS"])
+@cross_origin()
 def generate():
     data = request.get_json()
     ## BEGIN YOUR CODE
-    output = {} # PLACEHOLDER
+    output = "\ngraph TD\nA[Client] --> B[Load Balancer]\nB --> C[Server1]\nB --> D[Server2]" # PLACEHOLDER
     print(data) # PLACEHOLDER
     ## END YOUR CODE
     return output
+
